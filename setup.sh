0c9d2775f021d9f7bab16bff400e6a8ba536c744
Adeline Benoit <Adeline@MacBook-Pro.local>
2021-02-09 15:25:46 +0100

    ok

--- setup.sh
+++ setup.sh
@@ -45,6 +45,12 @@ start_project()
     # eval $(minikube docker-env -u)
 }
 
+clean_service()
+{
+    docker image rm -f $1_im
+    kubectl delete -f srcs/$1/$1.yaml
+}
+
 clean_project()
 {
     eval $(minikube docker-env -u)
@@ -52,14 +58,77 @@ clean_project()
     docker rmi -f $(docker image ls -a -q)
 }
 
-if [ $1 -eq "restart"]
-then
-    echo "\033[1;39m$2 restarting ... \033[0m"
-    if [$2 -eq "minikube"]
-    then
+restart_it()
+{
+    if [ $1 = "minikube" ]
+        then
         clean_project
         start_project
-    
+    elif [ $1 = "all" ]
+        then
+        kubectl delete -f srcs/metallb/metallb-configmap.yaml
+        clean_service nginx
+        kubectl apply -f srcs/metallb/metallb-configmap.yaml
+        start_service nginx
+    elif [ $1 -ne 0 ]
+        then
+        clean_service $1
+        start_service $1
+    else
+        echo "restart: $1: no such service"
+    fi
+}
+
+clean_it()
+{
+    if [ $1 = "all" ]
+        then
+        clean_all
+    elif [ $1 = "minikube" ]
+        then
+        clean_project
+    elif [ $# -ne 1 ]
+        then
+        clean_service $?
+    else
+        echo "clean: $1: no such service"
+    fi
+}
+
+start_it()
+{
+    if [ $1 = "all" ]
+        then
+        start_all
+    elif [ $1 = "minikube" ]
+        then
+        start_project
+    elif [ -z "$1" ]
+        then
+        echo "start: $1: no such service"
+    else
+        start_service $1
+    fi
+}
+
+if [ $1 = "restart" ]
+    then
+    echo "\033[1;39m$2 restarting ... \033[0m"
+    restart_it $2
+elif [ $1 = "clean" ]
+    then
+    echo "\033[1;39m$2 cleaning ... \033[0m"
+    clean_it $2
+elif [ $1 = "start" ]
+    then
+    echo "\033[1;39m$2 starting ... \033[0m"
+    start_it $2
+else
+    echo "$0: illegal option -- $1"
+    echo "usage: $0"
+    echo "      - clean [arg]"
+    echo "      - start [arg]"
+    echo "      - restart [arg]"
 fi
 
 # cd mysql/
