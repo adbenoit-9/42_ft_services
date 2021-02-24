288a873b65ea6e1a5c0a170688f29c7b12548380
adbenoit-9 <adeline.benoit@hotmail.com>
2021-02-24 15:54:34 +0100

    grrr

--- srcs/phpmyadmin/config.sample.inc.php
+++ srcs/phpmyadmin/config.sample.inc.php
@@ -29,7 +29,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'wordpress';
+$cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
