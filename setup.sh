f31fc08e93af2f99f4f142ce22a5adb9b861bc7f
adbenoit-9 <adeline.benoit@hotmail.com>
2021-03-15 16:38:29 +0100

    design setup

--- setup.sh
+++ setup.sh
@@ -13,42 +13,52 @@
 # docker-machine start
 # minikube start --vm-driver=virtualbox
 
+progress_anim()
+{
+    echo -n "    "
+    while :;
+    do
+        echo -n "\b\b\b\b[  ]"
+        sleep 0.5
+        echo -n "\b\b\b\b[. ]"
+        sleep 0.5
+        echo -n "\b\b\b\b[..]"
+        sleep 0.5
+    done
+}
+
 start_service()
 {
     eval $(minikube docker-env)
-    docker build -t $1_im srcs/$1
+    echo -n "\033[36;1m$1 \033[0;1m"
+    progress_anim & pro_pid=$!
+    echo -n "\033[0m"
+    docker build -t $1_im srcs/$1 > /dev/null
     if [ $? -eq 0 ]
     then
-        kubectl apply -f srcs/$1/$1.yaml
+        kubectl apply -f srcs/$1/$1.yaml > /dev/null
+        status=$?
+        kill $pro_pid
+        wait $pro_pid
     else
-        echo "\033[34m$1 \033[0m[\033[31mKO\033[0m]"
+        kill $pro_pid
+        wait $pro_pid
+        echo "\r\033[36;1m$1 image \033[0;1m[\033[31;1mKO\033[0;1m]\033[0m"
         exit 1
     fi
-    if [ $? -eq 0 ]
+    if [ $status -eq 0 ]
     then
-        echo "\033[34m$1 \033[0m[\033[32mOK\033[0m]"
+        echo "\r\033[36;1m$1 \033[0;1m[\033[32;1mOK\033[0;1m]\033[0m"
     else
-        echo "\033[34m$1 \033[0m[\033[31mKO\033[0m]"
+        echo "\r\033[36;1m$1 deployment\033[0;1m[\033[31;1mKO\033[0;1m]\033[0m"
     fi
     eval $(minikube docker-env -u)
 }
 
-start_project()
+start_all()
 {
-    # sudo usermod -aG docker user42; newgrp docker
-    minikube start --vm-driver=docker
-    if [ $? -ne 0 ]
-    then
-        echo '\033[1;33mminikube: \033[1;39mstarted [\033[1;31mKO\033[1;39m]\033[0m\nexit'
-        exit 1
-    fi
-    echo '\033[1;33mminikube: \033[1;39mstarted [\033[1;32mOK\033[1;39m]\033[0m'
-    minikube addons enable dashboard
-    minikube addons enable metrics-server
-    minikube addons enable metallb 
-    kubectl apply -f srcs/metallb/metallb.yaml
-    kubectl apply -f srcs/metallb/metallb-configmap.yaml
-    echo '\033[34mLoad Balancer \033[0m[\033[32mOK\033[0m]'
+    kubectl apply -f srcs/metallb/metallb-configmap.yaml > /dev/null
+    echo '\033[34;1mLoad Balancer \033[0;1m[\033[32mOK\033[0;1m]\033[0m\n'
     start_service nginx
     start_service mysql
     start_service phpmyadmin
@@ -59,25 +69,52 @@ start_project()
     start_service grafana
 }
 
+start_project()
+{
+    # sudo usermod -aG docker user42; newgrp docker
+    echo -n "\033[1;33mminikube: \033[1;39mstarting \033[1;0m"
+    progress_anim & pro_pid=$!
+    echo -n "\033[0m"
+    minikube start --vm-driver=docker > /dev/null
+    status=$?
+    kill $pro_pid
+    wait $pro_pid
+    if [ $status -ne 0 ]
+    then
+        echo '\033[1;33mminikube: \033[1;39mstarted [\033[1;31mKO\033[1;39m]\033[0m\nexit'
+        exit 1
+    fi
+    echo
+    echo '\033[1;33mminikube: \033[1;39mstarted [\033[1;32mOK\033[1;39m]\033[0m \n'
+    minikube addons enable dashboard > /dev/null
+    minikube addons enable metrics-server > /dev/null
+    minikube addons enable metallb > dev/null
+    kubectl apply -f srcs/metallb/metallb.yaml > /dev/null
+    start_all
+}
+
 clean_service()
 {
     eval $(minikube docker-env)
-    docker image rm -f $1_im
-    kubectl delete -f srcs/$1/$1.yaml
+    docker image rm -f $1_im > /dev/null
+    kubectl delete -f srcs/$1/$1.yaml > /dev/null
+    echo "\033[36;1m$1 \033[0;1mdeleted\033[0m"
     eval $(minikube docker-env -u)
 }
 
 clean_project()
 {
-    minikube delete
-    docker container stop $(docker container ls -aq)
-    docker container rm $(docker container ls -aq)
-    docker rmi -f $(docker image ls -a -q)
+    minikube delete > /dev/null
+    # docker container stop $(docker container ls -aq) > /dev/null
+    # docker container rm $(docker container ls -aq) > /dev/null
+    docker rmi -f $(docker image ls -a -q) > /dev/null
+    echo "\033[33;1mminikube \033[0;1mdeleted\033[0m"
 }
 
 clean_all()
 {
     kubectl delete -f srcs/metallb/metallb-configmap.yaml
+    echo "\033[36;1mLoad Balancer \033[0;1mdeleted\033[0m"
     clean_service nginx
     clean_service mysql
     clean_service phpmyadmin
@@ -86,19 +123,6 @@ clean_all()
     clean_service influxdb
     clean_service telegraf
     clean_service grafana
-}
-
-start_all()
-{
-    kubectl apply -f srcs/metallb/metallb-configmap.yaml
-    start_service nginx
-    start_service mysql
-    start_service phpmyadmin
-    start_service wordpress
-    start_service ftps
-    start_service influxdb
-    start_service telegraf
-    start_service grafana
 }
 
 restart_it()
@@ -153,17 +177,21 @@ start_it()
     fi
 }
 
+echo "\033[34m"
+cat ft_services.txt
+echo "\033[0m"
+
 if [ $1 = "restart" ]
     then
-    echo "\033[1;39m$2 restarting ... \033[0m"
+    echo "\033[1;33m$2 restarting ... \033[0m"
     restart_it $2
 elif [ $1 = "clean" ]
     then
-    echo "\033[1;39m$2 cleaning ... \033[0m"
+    echo "\033[1;33m$2 cleaning ... \033[0m"
     clean_it $2
 elif [ $1 = "start" ]
     then
-    echo "\033[1;39m$2 starting ... \033[0m"
+    echo "\033[1;33m$2 starting ... \033[0m"
     start_it $2
 else
     echo "$0: illegal option -- $1"
@@ -171,4 +199,9 @@ else
     echo "      - clean [arg]"
     echo "      - start [arg]"
     echo "      - restart [arg]"
+    echo ""
+    echo "arg:"
+    echo "      - minikube      (apply to all the project)"
+    echo "      - service name  (apply to the service)"
+    echo "      - all           (apply to all services without restart minikube)"
 fi
